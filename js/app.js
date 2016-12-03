f2e2bcf7bb6a08f7e54c628beb4b4ded8829a306
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-03 17:42:44 -0500

    added changes to set timeout and got it to cycle between components

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-'use strict';
-
 document.addEventListener("DOMContentLoaded", function(event) {
 console.log("DOM fully loaded and parsed");
 // pointer at span tag with class
-const $span = document.querySelector('.swap-name');
+const rotateWord = document.querySelector('.swap-name');
 
-const arrayOfDescriptions = ['Developer', 'Tinkerer', 'Designer', 'Foodie'];
+const arrayOfDescriptions = ['Developer', 'Tinkerer', 'Designer', 'Foodie', '420 guru'];
 // FUNCTION: changes randomly the value of a span tag in landing
 function changeSpan() {
   // debugger
-  $span.innerText = arrayOfDescriptions[Math.floor(Math.random() * 3)];
-  console.log('changed');
+
+  let intervalID = setInterval(() => {
+    rotateWord.innerText = arrayOfDescriptions[Math.floor(Math.random() * arrayOfDescriptions.length)];
+    // console.log('changed');
+  }, 3000) // end of setinterval
+
 }
 
 changeSpan();
