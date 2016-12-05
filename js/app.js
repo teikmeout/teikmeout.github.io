370dc82c5bbc5bb10fc073afdbc52d99782905a5
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-12-04 21:41:44 -0500

    added some changes to make setinterval work again. commented out fade out code.im kinda giving up and maybe will use jquery

--- js/app.js
+++ js/app.js
@@ -3,28 +3,29 @@ console.log("DOM fully loaded and parsed");
 // pointer at span tag with class
 const rotateWord = document.querySelector('.swap-name');
 
-const arrayOfDescriptions = ['Developer', 'Tinkerer', 'Designer', 'Foodie'];
+const arrayOfDescriptions = ['Developer', 'Tinkerer', 'Designer', 'Foodie',];
 // FUNCTION: changes randomly the value of a span tag in landing
 function changeSpan() {
   // debugger
 
   let intervalID = setInterval(() => {
+    console.log('change!')
     rotateWord.innerText = arrayOfDescriptions[Math.floor(Math.random() * arrayOfDescriptions.length)];
     // console.log('changed');
   }, 3000) // end of setinterval
 
 }
 
-function fade(thing, direction) {
-  let opacity = 1;
-  let intervalID2 = setInterval(() => {
-    if (opacity === 0 ) {
-      clearInterval(intervalID2);
-      element.
-    }
-    thing.style.opacity =
-  }, 50)
-}
+// function fade(thing, direction) {
+//   let opacity = 1;
+//   let intervalID2 = setInterval(() => {
+//     if (opacity === 0 ) {
+//       clearInterval(intervalID2);
+//       // element.
+//     }
+//     thing.style.opacity =
+//   }, 50)
+// }
 
 changeSpan();
 
