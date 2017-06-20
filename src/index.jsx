80c9cf933547334c21256a3a8e4a63558cc3d24d
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-06-19 20:46:29 -0400

    changes to anchor and root container

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+// anchor file for react components
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+// import './styles/index.css';
+// import './styles/normalize.css';
+
+ReactDOM.render(<App />, document.querySelector("#root-container"));
