d1caad665372de433b465b8d0397f1fd902e0d30
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-06-20 15:00:56 -0400

    modified views

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -11,13 +11,48 @@ class App extends React.Component {
     }
   } // end of contructor
 
+  toggleHiddenProject(e) {
+    console.log(e.target.nextSibling);
+    e.target.nextSibling.classList.toggle('hidden');
+  }
+
   render() {
     return(
       <div className="container">
         <h1>Jonathan Ahrens</h1>
         <h3>Full Stack Web Developer</h3>
-        <small>Brooklyn, NY</small> <br/>
+        <code>Brooklyn, NY</code> <br />
         <a href="tel:917-302-5196" className="phone"><small>917-3025196</small></a>
+        <h4>Projects</h4>
+        <ul>
+          <li onClick={(e) => this.toggleHiddenProject(e)}>Minesweeper</li>
+            <div className="project-container hidden">
+
+            </div>
+          <li>Crowd Sourcing Bus Mapper</li>
+            <div className="project-container hidden">
+
+            </div>
+          <li>Reask, classroom polling</li>
+            <div className="project-container hidden">
+
+            </div>
+          <li>Parking Meter Concept App</li>
+            <div className="project-container hidden">
+
+            </div>
+        </ul>
+
+        <h4>Skills</h4>
+        <ul>
+          <li>Javascript</li>
+          <li>React.js</li>
+          <li>Webpack</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+          <li>Ruby</li>
+          <li>Ruby on Rails</li>
+        </ul>
       </div>
     )
   }
