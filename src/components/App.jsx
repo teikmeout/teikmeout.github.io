94fc23c9f1c8b7eb574f09fc4426148680b6f103
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-07-31 14:31:45 -0400

    changed layout of new skills

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 // import ReactTooltip from 'react-tooltip';
-import './App.css';
-import '../styles/normalize.css';
+// import '../styles/normalize.css';
+import '../styles/App.css';
+import {renderSkills} from './Skills.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -37,7 +38,7 @@ class App extends React.Component {
 
         <h4>Projects</h4>
         <div  className="proj-title">
-          <p onClick={(e) => this.toggleHiddenProject(e)}>Reask, classroom polling</p>
+          <p onClick={(e) => this.toggleHiddenProject(e)}>🥑  Reask, classroom polling</p>
         </div>
           <div className="project-container hidden">
             <div className="description-container border">description of the project</div>
@@ -45,7 +46,7 @@ class App extends React.Component {
           </div>
 
         <div className="proj-title">
-          <p onClick={(e) => this.toggleHiddenProject(e)}>Crowd Sourcing Bus Mapper</p>
+          <p onClick={(e) => this.toggleHiddenProject(e)}>🥑  Crowd Sourcing Bus Mapper</p>
         </div>
           <div className="project-container hidden">
             <div className="description-container border">description of the project</div>
@@ -53,7 +54,7 @@ class App extends React.Component {
           </div>
 
         <div className="proj-title">
-          <p onClick={(e) => this.toggleHiddenProject(e)}>Parking Meter Concept App</p>
+          <p onClick={(e) => this.toggleHiddenProject(e)}>🥑  Parking Meter Concept App</p>
         </div>
           <div className="project-container hidden">
             <div className="description-container border">description of the project</div>
@@ -61,35 +62,19 @@ class App extends React.Component {
           </div>
 
         <div className="proj-title">
-          <p onClick={(e) => this.toggleHiddenProject(e)}>Minesweeper</p>
+          <p onClick={(e) => this.toggleHiddenProject(e)}>🥑  Minesweeper</p>
         </div>
           <div className="project-container hidden">
             <div className="description-container border">description of the project</div>
             <div className="image-container border">image of the project</div>
           </div>
 
-        <h4>Skills</h4>
-        <ul>
-          <li>Javascript</li>
-          <li>jQuery</li>
-          <li>AJAX</li>
-          <li>React.js</li>
-          <li>React-Native.js</li>
-          <li>Webpack 2.0</li>
-          <li>Node.js</li>
-          <li>Restful Routing</li>
-          <li>Fetch</li>
-          <li>Express.js</li>
-          <li>Ruby</li>
-          <li>Ruby on Rails</li>
-        </ul>
+        {renderSkills()}
 
         <h4>Contact me</h4>
-        <ul>
-          <li>Linkedin: jonathan-ahrens</li>
-          <li>Github: teikmeout</li>
-          <li>Email: ahrens.jonathan@gmail.com</li>
-        </ul>
+          <div>🍥 Linkedin: jonathan-ahrens</div>
+          <div>🍥 Github: teikmeout</div>
+          <div>🍥 Email: ahrens.jonathan@gmail.com</div>
       </div>
     )
   }
