f6b33591d6b9f5d3eac17335aea6aa57d98bc709
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-08-01 16:13:29 -0400

    changes that fucked everything up

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-// import './styles/index.css';
-
-ReactDOM.render(<App />, document.querySelector("#root-container"));
