e6080630dfcbabf50962dfb78af153735771503d
Jonathan Ahrens <ahrens.jonathan@gmail.com>
2018-11-21 12:28:31 -0500

    Containerizing Gists

---
+++ src/Gists/Gists.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Gists(props) {
+  console.log('props in Gists -> ', props.gists);
+  return (
+    <section>
+      <h3>Gists</h3>
+      {
+        props.gists.length > 0
+          ? props.gists.map(x => {
+              for (let llave in x.files) {
+                return(
+                  <a href={x.html_url} key={llave}><p key={"p"+llave}>{llave}</p></a>
+                );
+              }
+          })
+          : <h1>{"-"+props.gists.length}</h1>
+       }
+    </section>
+  )
+}
+
+export default Gists;
