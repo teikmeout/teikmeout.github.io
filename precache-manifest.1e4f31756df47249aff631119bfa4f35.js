5841557c1cae2fcef1b7dc2138fe7e6f11727cbe
Jonathan Ahrens <ahrens.jonathan@gmail.com>
2018-11-29 11:38:00 -0500

    Updates

---
+++ precache-manifest.1e4f31756df47249aff631119bfa4f35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b50b308a7de09b21bf3e",
+    "url": "/static/js/main.b50b308a.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "b50b308a7de09b21bf3e",
+    "url": "/static/css/main.c63ceac8.chunk.css"
+  },
+  {
+    "revision": "294243570731341aaedd1207ab3d8281",
+    "url": "/index.html"
+  }
+];
