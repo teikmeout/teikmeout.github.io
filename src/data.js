f4ed77aea8462676ae47d7913004027c970e8832
Jonathan Ahrens <ahrens.jonathan@gmail.com>
2018-11-29 10:54:55 -0500

    Refactor Data

--- src/data.js
+++ src/data.js
@@ -1,44 +1,42 @@
-module.exports = {
-  "projects": [
-    {
-      "name": "Reask, classroom polling",
-      "desc": "Built a realtime web-app for use in classrooms that aggregates students questions giving teachers better insight into relevant topics in real time.",
-      "tech": "Node.js + Express.js + React.js + Socket.io + PostgreSQL.",
-      "imageurl": "image not available",
-      "link": "http://re-ask.herokuapp.com/",
-      "github": "https://github.com/teikmeout/reAsk",
-    },
-    {
-      "name": "Crowd Sourcing Bus Mapper",
-      "desc": "A web application that allows users to contribute to a crowdsourced map of bus-stops that is much simpler than municipal maps.",
-      "tech": "Node.js + Express.js + MongoDB + Google Maps API + geolocation.",
-      "imageurl": "image not available",
-      "link": "https://cs-busstop.herokuapp.com/",
-      "github": "https://github.com/teikmeout/CrowdSourcedBusStops",
-    },
-    {
-      "name": "Parking Meter Concept App",
-      "desc": "Created a web application that serves as a digital parking meter system.",
-      "tech": "Node.js + Express.js + React.js + Google Maps API + PostgreSQL.",
-      "imageurl": "image not available",
-      "link": "http://iparkmyself.herokuapp.com/",
-      "github": ",https://github.com/teikmeout/iParkMyself",
-    },
-    {
-      "name": "Grace Hopper Slack Bot",
-      "desc": "Contributed to a Slackbot that allows students to queue up with TA’s and manages what instructor helps them out.",
-      "tech": "Node.js + Slackbot + MongoDB.",
-      "imageurl": "image not available",
-      "link": "not deployed",
-      "github": "https://github.com/teikmeout/gracehopper",
-    },
-    {
-      "name": "Youtube video syncer",
-      "desc": "Application that allows two users to watch a youtube video at the same time by  synchronizing  of times and actions.",
-      "tech": "Sockets.io + Node.js + Express.js.",
-      "imageurl": "image not available",
-      "link": "not deployed",
-      "github": "https://github.com/teikmeout/youtubeSocketSync"
-    },
-  ]
-}
+export default[
+  {
+    "name": "Reask, classroom polling",
+    "desc": "Built a realtime web-app for use in classrooms that aggregates students questions giving teachers better insight into relevant topics in real time.",
+    "tech": "Node.js + Express.js + React.js + Socket.io + PostgreSQL.",
+    "imageurl": "image not available",
+    "link": "http://re-ask.herokuapp.com/",
+    "github": "https://github.com/teikmeout/reAsk",
+  },
+  {
+    "name": "Crowd Sourcing Bus Mapper",
+    "desc": "A web application that allows users to contribute to a crowdsourced map of bus-stops that is much simpler than municipal maps.",
+    "tech": "Node.js + Express.js + MongoDB + Google Maps API + geolocation.",
+    "imageurl": "image not available",
+    "link": "https://cs-busstop.herokuapp.com/",
+    "github": "https://github.com/teikmeout/CrowdSourcedBusStops",
+  },
+  {
+    "name": "Parking Meter Concept App",
+    "desc": "Created a web application that serves as a digital parking meter system.",
+    "tech": "Node.js + Express.js + React.js + Google Maps API + PostgreSQL.",
+    "imageurl": "image not available",
+    "link": "http://iparkmyself.herokuapp.com/",
+    "github": ",https://github.com/teikmeout/iParkMyself",
+  },
+  {
+    "name": "Grace Hopper Slack Bot",
+    "desc": "Contributed to a Slackbot that allows students to queue up with TA’s and manages what instructor helps them out.",
+    "tech": "Node.js + Slackbot + MongoDB.",
+    "imageurl": "image not available",
+    "link": "not deployed",
+    "github": "https://github.com/teikmeout/gracehopper",
+  },
+  {
+    "name": "Youtube video syncer",
+    "desc": "Application that allows two users to watch a youtube video at the same time by  synchronizing  of times and actions.",
+    "tech": "Sockets.io + Node.js + Express.js.",
+    "imageurl": "image not available",
+    "link": "not deployed",
+    "github": "https://github.com/teikmeout/youtubeSocketSync"
+  }
+]
