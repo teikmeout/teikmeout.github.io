b2217cd86005906bdd6fc6cdd9c37b00bc7168d7
Jonathan Ahrens <ahrens.jonathan@gmail.com>
2018-11-29 10:45:15 -0500

    maped gist content to obtain gist titles

--- src/Gists/Gists.jsx
+++ src/Gists/Gists.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 
 function Gists(props) {
-  console.log('props in Gists -> ', props.gists);
   return (
     <section>
       <h3>Gists</h3>
       {
+        props &&
+        props.gists &&
         props.gists.length > 0
-          ? props.gists.map(x => {
-              for (let llave in x.files) {
-                return(
-                  <a href={x.html_url} key={llave}><p key={"p"+llave}>{llave}</p></a>
-                );
-              }
+          ? props.gists.map(gist => {
+            const filenamekeys = Object.keys(gist.files)
+            const gistTitles = filenamekeys.map(filename => {
+              const firstLinebBreak = gist.files[filename].content.indexOf('\n')
+              const title = gist.files[filename].content.substring(1, firstLinebBreak)
+
+              return (
+                <a href={gist.html_url} key={filename}>
+                  <p key={"p"+ filename}>{title}</p>
+                </a>
+              )
+            })
+            return gistTitles
           })
-          : <h1>{"-"+props.gists.length}</h1>
-       }
+          : <h1>loading gists...</h1>
+      }
     </section>
   )
 }
