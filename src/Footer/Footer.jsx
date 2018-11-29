df82499637efe3c6bf7ab574de758357e1681098
Jonathan Ahrens <ahrens.jonathan@gmail.com>
2018-11-29 11:31:42 -0500

    Rename file for footer css

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import './Footer.css';
+import './Footer.scss';
 
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <h3>Contact</h3>
       <div className="contact-info"><span aria-label="avo" role="img">🍥</span> Linkedin: <a className="bottom-link" href="https://www.linkedin.com/in/ahrens-jonathan/">ahrens-jonathan</a></div>
       <div className="contact-info"><span aria-label="avo" role="img">🍥</span> Github: <a className="bottom-link" href="https://github.com/teikmeout">teikmeout</a></div>
       <div className="contact-info"><span aria-label="avo" role="img">🍥</span> Email: <a className="bottom-link" href="mailto:ahrens.jonathan@gmail.com">ahrens.jonathan@gmail.com</a></div>
       <br/>
-      <small>Finding how to deploy this baby was complex. Learn about create-react-app and "gh-pages user page" on my <a href="https://github.com/teikmeout/teikmeout.github.io/blob/create-react-app/README.md">README</a></small>
+      <small>Learn about deploying create-react-app and "gh-pages user page" on my <a href="https://github.com/teikmeout/teikmeout.github.io/blob/create-react-app/README.md">README</a></small>
     </footer>
   )
 }
