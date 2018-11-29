b3341a6bc543d83ad3ffba67063abf4218b8aa50
Jonathan Ahrens <ahrens.jonathan@gmail.com>
2018-11-29 10:54:38 -0500

    Convert to SASS content

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -2,35 +2,35 @@ import React from 'react';
 import './Projects.css';
 
 function Projects(props) {
-
-  console.log('running projects! ', props);
   return (
     <section>
       <h3>Projects</h3>
-      {props.projects.map((el, i) => {
-        return (
-          <div key={"cont"+i} className="whole-proj">
-            <div  className="proj-title" key={"proj-title"+i}>
-              <h4 key={"title"+i} className="proj-h4"><span key={"avo"+i} aria-label="avo" role="img">🥑 </span>{el.name}</h4>
+      <div className="project-container">
+        {props.projects.map((el, i) => {
+          return (
+            <div key={"cont"+i} className="whole-proj">
+              <div  className="proj-title" key={"proj-title"+i}>
+                <h4 key={"title"+i} className="proj-h4"><span key={"avo"+i} aria-label="avo" role="img">🥑 </span>{el.name}</h4>
+              </div>
+              <div key={"proj-cont"+i} className="proj-cont hidden">
+                <div key={"desc-cont"+i} className="desc-cont">
+                  {el.desc}
+                </div>
+                <div>
+                  <small>- {el.tech}</small><br/>
+                </div>
+                <div className="proj-link">
+                  <small><a href={el.github}>{el.link}</a></small>
+                </div>
+                <div className="proj-gh">
+                  <small><a className="link" href={el.github}>{el.github}</a></small>
+                </div>
+                {/*<div key={"img-cont"+i} className="img-cont"><small key={"small-img-desc"+i}>{el.imageurl}</small></div>*/}
+              </div>
             </div>
-            <div key={"proj-cont"+i} className="proj-cont hidden">
-              <div key={"desc-cont"+i} className="desc-cont">
-                {el.desc}
-              </div>
-              <div>
-                <small>- {el.tech}</small><br/>
-              </div>
-              <div className="proj-link">
-                <small><a href={el.github}>{el.link}</a></small>
-              </div>
-              <div className="proj-gh">
-                <small><a className="link" href={el.github}>{el.github}</a></small>
-              </div>
-              {/*<div key={"img-cont"+i} className="img-cont"><small key={"small-img-desc"+i}>{el.imageurl}</small></div>*/}
-            </div>
-          </div>
-        )
-      })}
+          )
+        })}
+      </div>
 
     </section>
   )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './normalize.css'
-import './index.css';
+import './sass/main.scss';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 
